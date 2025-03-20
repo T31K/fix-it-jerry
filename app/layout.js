1b2448372bf96b1c530555475371bc8f1c7a8554
T31K <t31kmunwong@gmail.com>
2025-03-20 17:46:05 +0800

    try

--- app/layout.js
+++ app/layout.js
@@ -35,41 +35,18 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${manrope.variable} antialiased`}>
         {children}
+        <Script src="/js/plugins.js" strategy="beforeInteractive"></Script>
 
-        {/* Ensure jQuery loads first */}
+        <Script src="/js/swiper.js" strategy="afterInteractive"></Script>
         <Script
-          src="https://code.jquery.com/jquery-3.6.0.min.js"
-          strategy="beforeInteractive"
-          onLoad={() => console.log("jQuery Loaded")}
-        />
-
-        {/* Load dependent plugins after jQuery */}
-        <Script
-          src="/js/jquery.magnific-popup.min.js"
+          src="/js/custom-marquee.js"
           strategy="afterInteractive"
-          onLoad={() => {
-            if (typeof $.fn.magnificPopup === "undefined") {
-              console.error("Magnific Popup did not load correctly.");
-            } else {
-              console.log("Magnific Popup Loaded");
-            }
-          }}
-        />
+        ></Script>
         <Script
-          src="/js/owl.carousel.min.js"
+          src="/js/custom-swiper-1.js"
           strategy="afterInteractive"
-          onLoad={() => {
-            if (typeof $.fn.owlCarousel === "undefined") {
-              console.error("Owl Carousel did not load correctly.");
-            } else {
-              console.log("Owl Carousel Loaded");
-            }
-          }}
-        />
-
-        {/* Finally, load your custom scripts */}
-        <Script src="/js/plugins.js" strategy="afterInteractive" />
-        <Script src="/js/designesia.js" strategy="afterInteractive" />
+        ></Script>
+        <Script src="/js/designesia.js" strategy="afterInteractive"></Script>
       </body>
     </html>
   );
