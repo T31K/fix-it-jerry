16256b1df8127cbc8495f27a21602eed1739a47e
T31K <t31kmunwong@gmail.com>
2025-06-04 14:50:02 +0800

    try iframe

---
+++ app/analytics/page.jsx
@@ -0,0 +1,9 @@
+function page() {
+    return (
+        <main className="w-[100vw] h-[100vh]">
+        <iframe src="https://umami.t31k.cloud/share/nA85iHszIVF8c8cQ/fixitjerry.com" width="100%" height="100%" />
+        </main>
+    )
+}
+
+export default page
